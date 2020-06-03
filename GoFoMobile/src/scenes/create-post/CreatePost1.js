6d6de009192bdffba66ba3491c412196b9b21489
Nhat Minh <nhatminhn900@gmail.com>
2020-06-03 23:50:50 +0700

    uploaded single image with fetch

--- GoFoMobile/src/scenes/create-post/CreatePost1.js
+++ GoFoMobile/src/scenes/create-post/CreatePost1.js
@@ -1,34 +1,83 @@
-import React, {useEffect,useState,useCallback} from 'react'
-import {View, Text, CameraRoll} from 'react-native'
-import Header from '../../components/Header'
+import React, {useEffect, useState, useCallback} from 'react';
+import {View, Text, TouchableOpacity,StyleSheet} from 'react-native';
+import Header from '../../components/Header';
 import {Button} from 'react-native-elements';
+import ImagePicker from 'react-native-image-picker';
+import * as api from '../../services/home';
 
-
-import * as api from "../../services/home";
-
-import LocalizationContext from "../../localization/LocalizationContext";
-
-
-
-
+import LocalizationContext from '../../localization/LocalizationContext';
 
 
 //Product Page1  {t('welcome')}
 function CreatePost1({navigation}) {
     const {t, i18n} = React.useContext(LocalizationContext);
-    const [photos, getPhotos] = useCameraRoll({ first: 80 });
 
+    let [singleFile, setSingleFile] = useState(null);
 
+    let uploadImage = async () => {
+        //Check if any file is selected or not
+        if (singleFile != null) {
+            //If file selected then create FormData
+            const fileToUpload = singleFile;
+            const data = new FormData();
+            //data.append('image.jpg', 'Image Upload');
+            data.append('images', fileToUpload);
 
-    async function uploadImage(images) {
-        try {
-            let response = await api.uploadImages(images)
-            console.log('uploadImage  ==> response ', response)
+            const file = {
+                uri: fileToUpload.uri,
+                name: fileToUpload.filename || Math.floor(Math.random() * Math.floor(999999999)) + '.jpg',
+                type: 'image/jpeg/jpg',
+                data: fileToUpload.data,
+            };
+            data.append('images', file);
 
-
-        } catch (error) {
-            console.log('uploadImage  error ', error)
+            //Please change file upload URL
+            let res = await fetch(
+                //'http://192.168.1.6/image-upload',
+                'https://morning-crag-89761.herokuapp.com/image-upload',
+                {
+                    method: 'post',
+                    body: data,
+                    headers: {
+                        'Content-Type': 'multipart/form-data; ',
+                    },
+                },
+            );
+            let responseJson = await res.json()
+            console.log('uploadImage  ==> ',responseJson)
+            if (responseJson.status == 1) {
+                alert('Upload Successful');
+            }
+        } else {
+            //if no file selected the show alert
+            alert('Please Select File first');
         }
+    };
+
+    async function selectFile() {
+        const options = {
+            title: 'Select Avatar',
+            storageOptions: {
+                skipBackup: true,
+                path: 'images',
+            },
+        };
+
+        ImagePicker.showImagePicker(options, (response) => {
+
+            if (response.didCancel) {
+                setSingleFile(null);
+                console.log('User cancelled image picker');
+            } else if (response.error) {
+                console.log('ImagePicker Error: ', response.error);
+            } else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton);
+            } else {
+                const source = {uri: response.uri};
+                setSingleFile(response);
+                console.log(' ********* ImagePicker success  *************   ', source.uri);
+            }
+        });
     }
 
     ;
@@ -36,16 +85,86 @@ function CreatePost1({navigation}) {
         <View>
             <Header titleText='Login'/>
             <Button
-                title="Upload images 1"
-                onPress={() => console.log("ahihi")}
+                title="Choose Image to upload"
+                onPress={() => pickUpImage()}
             />
 
-            <Text>
-                DKM
-            </Text>
+            <View style={{alignItems: 'center'}}>
+                <Text style={{fontSize: 30, textAlign: 'center'}}>
+                    React Native File Upload Example
+                </Text>
+                <Text
+                    style={{
+                        fontSize: 25,
+                        marginTop: 20,
+                        marginBottom: 30,
+                        textAlign: 'center',
+                    }}>
+
+                </Text>
+            </View>
+            {/*Showing the data of selected Single file*/}
+            {singleFile != null ? (
+                <Text style={styles.textStyle}>
+                    File Name: {singleFile.name ? singleFile.name : ''}
+                    {'\n'}
+                    Type: {singleFile.type ? singleFile.type : ''}
+                    {'\n'}
+                    File Size: {singleFile.size ? singleFile.size : ''}
+                    {'\n'}
+                    URI: {singleFile.uri ? singleFile.uri : ''}
+                    {'\n'}
+                </Text>
+            ) : null}
+            <TouchableOpacity
+                style={styles.buttonStyle}
+                activeOpacity={0.5}
+                onPress={selectFile}>
+                <Text style={styles.buttonTextStyle}>Select File</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={styles.buttonStyle}
+                activeOpacity={0.5}
+                onPress={uploadImage}>
+                <Text style={styles.buttonTextStyle}>Upload File</Text>
+            </TouchableOpacity>
+
 
         </View>
-    )
+    );
 }
 
-export default CreatePost1
+const styles = StyleSheet.create({
+    mainBody: {
+        flex: 1,
+        justifyContent: 'center',
+        padding: 20,
+    },
+    buttonStyle: {
+        backgroundColor: '#307ecc',
+        borderWidth: 0,
+        color: '#FFFFFF',
+        borderColor: '#307ecc',
+        height: 40,
+        alignItems: 'center',
+        borderRadius: 30,
+        marginLeft: 35,
+        marginRight: 35,
+        marginTop: 15,
+    },
+    buttonTextStyle: {
+        color: '#FFFFFF',
+        paddingVertical: 10,
+        fontSize: 16,
+    },
+    textStyle: {
+        backgroundColor: '#fff',
+        fontSize: 15,
+        marginTop: 16,
+        marginLeft: 35,
+        marginRight: 35,
+        textAlign: 'center',
+    },
+});
+
+export default CreatePost1;
--- GoFoMobile/src/services/home.js
+++ GoFoMobile/src/services/home.js
@@ -13,24 +13,26 @@ export async function getCategoryList() {
 }
 
 
-export async function uploadImages(images) {
+export async function uploadImages1(image) {
     try {
-        console.log('API uploadImages =============================> ', images.uri)
+        //console.log('API uploadImages =============================> ', images.uri)
+        console.log('uploadImages  images param 11 ==>  ', image.uri)
         const formData = new FormData();
-
-        Array.from(images).forEach(image => {
-            console.log('uploadImages  images  ==>  ', images)
-            formData.append('images', image.uri);
-        });
-        formData.append('images', images.uri);
-        console.log('uploadImages  ==>  formData ', formData)
+        const file = {
+            uri: image.uri,
+            name: image.filename || Math.floor(Math.random() * Math.floor(999999999)) + '.jpg',
+            type: 'image/jpeg/jpg',
+            data: image.data,
+        };
+        formData.append('image', file);
+        console.log('uploadImages formData  ==>   ', formData)
 
         let res = axios
             .post(c.UPLOAD_IMAGES, formData, {
                 headers: {'Content-Type': 'multipart/form-data'},
             })
             .then(res => {
-                console.log('uploadImages res',res.request.status);
+                console.log('uploadImages res ========>  ',res.status);
             })
             .catch(err => {
                 console.log('uploadImages err');
@@ -40,4 +42,46 @@ export async function uploadImages(images) {
     } catch (e) {
         throw handler(e)
     }
+
+
+
 }
+
+export async function uploadImages(images) {
+    //Check if any file is selected or not
+    if (singleFile != null) {
+        //If file selected then create FormData
+        const fileToUpload = singleFile;
+        const data = new FormData();
+        data.append('images', 'Image Upload');
+        data.append('images', fileToUpload);
+        let res = await fetch(
+            'https://morning-crag-89761.herokuapp.com/image-upload',
+            {
+                method: 'post',
+                body: data,
+                headers: {
+                    'Content-Type': 'multipart/form-data; ',
+                },
+            }
+        );
+        let responseJson = await res.json();
+        if (responseJson.status == 1) {
+            alert('Upload Successful');
+        }
+    } else {
+        //if no file selected the show alert
+        alert('Please Select File first');
+    }
+}
+
+
+// Array.from(images).forEach(image => {
+//     console.log('uploadImages  images param ==>  ', images)
+//     const file = {
+//         uri: image.path,
+//         name: image.filename || Math.floor(Math.random() * Math.floor(999999999)) + '.jpg',
+//         type: image.mime || 'image/jpeg'
+//     };
+//     formData.append('images', file);
+// });
