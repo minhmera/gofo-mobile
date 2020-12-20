401df3217b29855f4dc688bb4b4f3c2baf48ef8a
GoSELL_MacMini <minh.nhat.nguyen@mediastep.com>
2020-12-20 18:15:05 +0700

    styling product detail

--- GoFoMobile/src/scenes/product/ProductDetail.js
+++ GoFoMobile/src/scenes/product/ProductDetail.js
@@ -1,8 +1,7 @@
-
 import React, {useEffect, useState} from 'react'
 
 import {Text, FAB, List} from 'react-native-paper'
-import {FlatList, ImageBackground, RefreshControl, StyleSheet, TouchableOpacity, View} from 'react-native';
+import {ScrollView, FlatList, ImageBackground, RefreshControl, StyleSheet, TouchableOpacity, View} from 'react-native';
 import Header from '../../components/Header'
 import AsyncStorage from '@react-native-community/async-storage';
 import {TOKEN_KEY, USER_NAME_KEY} from "../../config/Contants";
@@ -20,25 +19,46 @@ function ProductDetail({navigation}) {
     console.log('MERA ProductDetail ==>  type: ', type);
 
 
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
-    const [selectedCity, setSelectedCity] = useState(null);
-    const [cities, setCities] = useState({});
-
+    const [sellingData, setSellingData] = useState(null);
+    const [buyingData, setBuyingData] = useState(null);
 
 
     function fetchSellingProductDetail() {
         api.getSellingProductDetail(productId).then((response) => {
-            console.log('MERA getSellingProductDetail : ',response.data.productName)
-
+            console.log('MERA getSellingProductDetail : ', response.data.productName)
+            setSellingData(response.data)
         });
     }
 
     function fetchBuyingProductDetail() {
         api.getBuyingProductDetail(productId).then((response) => {
-            console.log('MERA getSellingProductDetail : ',response.data.productName)
+            console.log('MERA getBuyingProductDetail : ', response.data.productName)
+            setBuyingData(response.data)
 
         });
+    }
+
+
+    function renderProductImages(item) {
+        if (item === null) {
+            return null
+        } else {
+            if (item.photoUrls === null) {
+                return null
+            }
+        }
+        /* At the moment just show 1  */
+        console.log('MERA  renderProductImages    ', item)
+        let images = item.photoUrls
+        let image = images[0]
+        return (
+            <View style={styles.productImageContainer}>
+                <ImageBackground style={styles.productImage} source={{uri: image}}>
+
+                </ImageBackground>
+                <View style={{height: 2, backgroundColor: GlobalStyle.colour.grayColor}}></View>
+            </View>
+        )
     }
 
     function getDefaultData() {
@@ -53,10 +73,99 @@ function ProductDetail({navigation}) {
         getDefaultData();
     }, []);
     return (
+
         <View style={styles.container}>
             <Header titleText='Product Detail' navigation={navigation}/>
+            <ScrollView>
+                {renderInfo()}
+            </ScrollView>
+
         </View>
+
     )
+
+    function renderInfo() {
+        if (type === 'SELLING') {
+            return (
+                <View style={styles.infoView}>
+                    {renderProductImages(sellingData)}
+                    {renderProductInfoView(sellingData)}
+                    {renderPersonInfoView(sellingData)}
+
+                </View>
+            )
+        } else {
+            return (
+                <View>
+
+                </View>
+            )
+        }
+    }
+
+    // Seller or Buyer info
+    function renderPersonInfoView(data) {
+        if (data === null) {
+            return null
+        }
+
+        let title = ''
+        if (type === 'SELLING') {
+            title = 'Thông tin người bán'
+        } else {
+            title = 'Thông tin người mua'
+        }
+
+
+        return (
+            <View style={styles.personView}>
+                <View style={styles.personWrapperView}>
+
+                    <View style={styles.titleView}>
+                        <View style={styles.separatorLineView}>
+                        </View>
+                        <Text style={styles.titleText} >{title}</Text>
+                    </View>
+
+                    <View style={styles.detailInfoView}>
+                        <Text>
+                            {data.fullName}
+                        </Text>
+                    </View>
+
+
+                </View>
+            </View>
+        )
+    }
+
+    function renderProductInfoView(data) {
+        if (data === null) {
+            return null
+        }
+        return (
+            <View style={styles.productView}>
+                <View style={styles.productWrapperView}>
+
+                    <View style={styles.titleView}>
+                        <View style={styles.separatorLineView}>
+                        </View>
+                        <Text style={styles.titleText} >Thông tin sản phâm</Text>
+                    </View>
+
+                    <View style={styles.detailInfoView}>
+                        <Text>
+                            {data.productName}
+                        </Text>
+                    </View>
+
+
+                </View>
+            </View>
+        )
+    }
+
+
 }
 
 export default ProductDetail
@@ -67,6 +176,62 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#F5F5F5',
     },
+    infoView: {
+        flex: 1
+    },
+
+    productImageContainer: {},
+    productImage: {
+        height: 300
+    },
+    personView: {
+        marginTop: 24,
+        height: 280,
+    },
+
+    personWrapperView: {
+        flex:1,
+        marginLeft: 8,
+    },
+
+    productView: {
+        marginTop: 24,
+        height: 280,
+    },
+
+    productWrapperView: {
+        flex:1,
+        marginLeft: 8,
+        marginRight:8
+
+    },
+
+    separatorLineView: {
+        width: 4,
+        height: 16,
+        backgroundColor: GlobalStyle.colour.primaryColor
+
+    },
+
+    titleView: {
+        flexDirection:'row'
+    },
+
+    titleText: {
+        marginLeft:4,
+        color:'#9E9E9E',
+        fontSize:16,
+        fontWeight:'bold'
+    },
+
+    detailInfoView: {
+        flex: 1,
+        marginTop: 8,
+        borderRadius:4,
+        backgroundColor: 'white',
+        borderWidth:0.5,
+        borderColor:GlobalStyle.colour.grayColor
+    }
 
 
 });
