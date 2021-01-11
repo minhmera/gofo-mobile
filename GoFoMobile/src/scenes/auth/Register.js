af4cacc11ecb81cde49b17281effaa8d37dbfd55
GoSELL_MacMini <minh.nhat.nguyen@mediastep.com>
2021-01-12 00:08:08 +0700

    stlying

--- GoFoMobile/src/scenes/auth/Register.js
+++ GoFoMobile/src/scenes/auth/Register.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {StatusBar, Alert, View, ScrollView, StyleSheet} from 'react-native';
+import {StatusBar, Alert, View, ScrollView, StyleSheet, TextInput, Text, TouchableOpacity} from 'react-native';
 
 import * as api from '../../services/auth';
 
@@ -10,7 +10,7 @@ import ProductPage1 from '../product/ProductPage1';
 import t from 'tcomb-form-native';
 import AppStyle from '../../style/style';
 import GlobalStyle from '../../style/GlobalStyle';
-import {Button,Input} from 'react-native-elements';
+import {Button, Input} from 'react-native-elements';
 import {KeyboardAwareScrollView} from 'react-native-keyboard-aware-scroll-view';
 
 import Icon from 'react-native-vector-icons/AntDesign';
@@ -39,47 +39,47 @@ function Register(props) {
 
     function isValidAllField() {
         let isValidAllFiled = true
-        if (userName == "" ) {
+        if (userName == "") {
             isValidAllFiled = false
-            setUserNameError({style:{borderBottomColor:'red'}, text:'User nam can not be empty'})
+            setUserNameError({style: {borderBottomColor: 'red'}, text: 'User nam can not be empty'})
         } else {
             isValidAllFiled = true
-            setUserNameError({style:{}, text:''})
+            setUserNameError({style: {}, text: ''})
         }
 
-        if (password == "" ) {
+        if (password == "") {
             isValidAllFiled = false
-            setPasswordError({style:{borderBottomColor:'red'}, text:'Password can not be empty'})
+            setPasswordError({style: {borderBottomColor: 'red'}, text: 'Password can not be empty'})
 
-        }else if (password.length < 8) {
-            setPasswordError({style:{borderBottomColor:'red'}, text:'Password must be more than 8 char '})
+        } else if (password.length < 8) {
+            setPasswordError({style: {borderBottomColor: 'red'}, text: 'Password must be more than 8 char '})
         } else {
             isValidAllFiled = true
-            setPasswordError({style:{}, text:''})
+            setPasswordError({style: {}, text: ''})
 
         }
 
-        if (confirmPass != password ) {
+        if (confirmPass != password) {
             isValidAllFiled = false
-            setConfirmPassError({style:{borderBottomColor:'red'}, text:'Confirm password is diff with password'})
+            setConfirmPassError({style: {borderBottomColor: 'red'}, text: 'Confirm password is diff with password'})
 
-        }else {
+        } else {
             isValidAllFiled = true
-            setConfirmPassError({style:{}, text:''})
+            setConfirmPassError({style: {}, text: ''})
 
         }
-        return  isValidAllFiled
+        return isValidAllFiled
     }
 
     async function onSubmit() {
-        console.log('MERA Register value: ',isValidAllField());
+        console.log('MERA Register value: ', isValidAllField());
         if (isValidAllField() === false) {
             return
         }
 
         let registerObj = {
-            "username":userName,
-            "password":password,
+            "username": userName,
+            "password": password,
 
         }
 
@@ -104,51 +104,63 @@ function Register(props) {
 
     return (
 
-            <View style={{flex: 1, backgroundColor: '#fff'}}>
-                <StatusBar barStyle="light-content" />
-                <Header titleText='Register' navigation={navigation}/>
+        <View style={{flex: 1, backgroundColor: '#fff'}}>
+            <StatusBar barStyle="light-content"/>
+            <Header titleText='Register' navigation={navigation}/>
 
 
-                <KeyboardAwareScrollView style={{flex: 1}} keyboardDismissMode = {'on-drag'}>
-                <View style={{flex: 1,marginTop:120,marginLeft:32, marginRight:32}}>
-                    <Input
-                        inputStyle={styles.inputStyle}
-                        inputContainerStyle={[styles.basicInput,userNameError.style]}
-                        //errorStyle={{ color: 'red' }}
-                        placeholder='User name'
-                        errorMessage={userNameError.text}
-                        onChangeText={text => setUserName(text)}
+            <KeyboardAwareScrollView style={{flex: 1}} keyboardDismissMode={'on-drag'}>
+                <View style={styles.loginContainer}>
+                    <View style={styles.inputView}>
+                        <Input
 
-                    />
-                    <Input
-                        inputStyle={styles.inputStyle}
-                        inputContainerStyle={[styles.basicInput,passwordError.style]}
-                        errorMessage={passwordError.text}
-                        placeholder='Password'
-                        onChangeText={text => setPassword(text)}
+                            inputStyle={styles.inputText}
+                            inputContainerStyle={[styles.inputContainer, userNameError.style]}
+                            placeholderTextColor={GlobalStyle.colour.grayColor2}
 
-                    />
-                    <Input
-                        inputStyle={styles.inputStyle}
-                        inputContainerStyle={[styles.basicInput,confirmPassError.style]}
-                        errorMessage={confirmPassError.text}
-                        placeholder='Confirm Password'
-                        onChangeText={text => setConfirmPass(text)}
+                            //errorStyle={{ color: 'red' }}
+                            //placeholderTextColor = {'red'}
+                            placeholder='Tên đăng nhập'
 
-                    />
+                            //errorMessage={userNameError.text}
+                            onChangeText={text => setUserName(text)}
 
-
-                    <View style={styles.bottomView}>
-                        <Button
-                            title="Đăng Ký"
-                            onPress={(state) => onSubmit(state)}
-                            buttonStyle={[AppStyle.commonButton, styles.submitButton]} //submitButton
-                            containerStyle={styles.buttonContainer}
                         />
                     </View>
+                    <View style={styles.inputView}>
+                        <Input
+                            inputStyle={styles.inputStyle}
+                            inputContainerStyle={[ styles.inputContainer,passwordError.style]}
+
+                            errorMessage={passwordError.text}
+                            placeholder='Password'
+                            placeholderTextColor={GlobalStyle.colour.grayColor2}
+                            onChangeText={text => setPassword(text)}
+
+                        />
+                    </View>
+                    <View style={styles.inputView}>
+                        <Input
+                            inputStyle={styles.inputStyle}
+                            inputContainerStyle={[styles.inputContainer, confirmPassError.style]}
+                            errorMessage={confirmPassError.text}
+                            placeholder='Confirm Password'
+                            placeholderTextColor={GlobalStyle.colour.grayColor2}
+                            onChangeText={text => setConfirmPass(text)}
+
+                        />
+                    </View>
+
+                    <TouchableOpacity
+                        style={styles.loginBtn}
+                        onPress={() => onSubmit()}
+                    >
+                        <Text style={styles.loginText}>Đăng ký</Text>
+                    </TouchableOpacity>
+
                 </View>
-                </KeyboardAwareScrollView>
-            </View>
+            </KeyboardAwareScrollView>
+        </View>
 
     );
 };
@@ -160,7 +172,12 @@ export default Register;
 //onPress={() => navigation.goBack()}
 
 const styles = StyleSheet.create({
-    bottomView: {},
+    loginContainer: {
+        flex: 1,
+        marginTop: 120,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
     buttonContainer: {
         paddingTop: 40,
         // paddingLeft: 32,
@@ -172,13 +189,97 @@ const styles = StyleSheet.create({
 
     },
     inputStyle: {
-        fontSize:16,
-        paddingTop:18,
+        fontSize: 16,
+        //paddingTop: 18,
         height: 58,
-        fontWeight:'400',
+        fontWeight: '400',
     },
-    basicInput: {
-        borderBottomWidth: 0.5,
-        borderBottomColor: '#B5B5B5',
+    inputContainer: {
+        borderBottomWidth:0
+    },
+
+    inputView: {
+        width: "80%",
+        //backgroundColor:"#465881",
+        borderWidth: 1,
+        borderColor: GlobalStyle.colour.primaryColor,
+        borderRadius: 25,
+        height: 50,
+        marginBottom: 20,
+        justifyContent: "center",
+        padding: 20
+    },
+    inputText: {
+        fontSize: 14,
+        height: 50,
+        color: GlobalStyle.colour.primaryColor
+    },
+    loginBtn: {
+        width: "80%",
+        //backgroundColor:"#fb5b5a",
+        backgroundColor: GlobalStyle.colour.primaryColor,
+        borderRadius: 25,
+        height: 50,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: 40,
+        marginBottom: 10
+    },
+    loginText: {
+        fontSize: 16,
+        fontWeight: '700',
+        color: 'white'
     },
 });
+
+
+/*
+return (
+
+    <View style={{flex: 1, backgroundColor: '#fff'}}>
+        <StatusBar barStyle="light-content" />
+        <Header titleText='Register' navigation={navigation}/>
+
+
+        <KeyboardAwareScrollView style={{flex: 1}} keyboardDismissMode = {'on-drag'}>
+            <View style={{flex: 1,marginTop:120,marginLeft:32, marginRight:32}}>
+                <Input
+                    inputStyle={styles.inputStyle}
+                    inputContainerStyle={[styles.basicInput,userNameError.style]}
+                    //errorStyle={{ color: 'red' }}
+                    placeholder='User name'
+                    errorMessage={userNameError.text}
+                    onChangeText={text => setUserName(text)}
+
+                />
+                <Input
+                    inputStyle={styles.inputStyle}
+                    inputContainerStyle={[styles.basicInput,passwordError.style]}
+                    errorMessage={passwordError.text}
+                    placeholder='Password'
+                    onChangeText={text => setPassword(text)}
+
+                />
+                <Input
+                    inputStyle={styles.inputStyle}
+                    inputContainerStyle={[styles.basicInput,confirmPassError.style]}
+                    errorMessage={confirmPassError.text}
+                    placeholder='Confirm Password'
+                    onChangeText={text => setConfirmPass(text)}
+
+                />
+
+
+                <View style={styles.bottomView}>
+                    <Button
+                        title="Đăng Ký"
+                        onPress={(state) => onSubmit(state)}
+                        buttonStyle={[AppStyle.commonButton, styles.submitButton]} //submitButton
+                        containerStyle={styles.buttonContainer}
+                    />
+                </View>
+            </View>
+        </KeyboardAwareScrollView>
+    </View>
+
+);*/
